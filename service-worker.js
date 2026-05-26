2417bc88a87fb92827615215cd2331e6969ad999
pipebidprocom-hub <pipebidpro.com@gmail.com>
2026-05-26 13:01:35 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "pipebid-pro-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./plumber-estimator.html",
+  "./manifest.webmanifest",
+  "./assets/icon.svg",
+  "./privacy-policy.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./plumber-estimator.html"))
+    )
+  );
+});
